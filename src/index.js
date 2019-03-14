cc2d3bf7ebf81d76ee28cf142a95d26ece676596
MaNi <mani.razavizadeh@gmail.com>
2019-03-14 16:50:29 +0330

    part 2

--- src/index.js
+++ src/index.js
@@ -3,17 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'tachyons';
 //import App from './App';
-import Card from './Card';
+import CardList from './CardList';
 import { robots } from './robots';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <div>
-    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
-    <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
-    <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
-  </div>
-
+  <CardList robots={robots} />
 , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
